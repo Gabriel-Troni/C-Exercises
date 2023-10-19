f271e8f4a9eaf62bf3229ca3b8afe267add5f5dd
Gabriel Troni <troni.gabriel.10@gmail.com>
2023-10-19 13:10:06 -0500

    ex1 finished

--- PHP/ex1/index.php
+++ PHP/ex1/index.php
@@ -17,37 +17,53 @@ function create_table_cell($txt){
 }
 
 function create_table_row($cells){
+  global $n;
   $result = "<tr>";
   for($i=0; $i<=$n; $i++){
-    $result .= create_table_cell($cells[i]);
+    $result .= create_table_cell($cells[$i]);
   }
   $result .= "</tr>";
   return($result);
 }
 
 function create_table($rows){
+  global $n, $matriz;
+  $result = "<table class='table'>";
+  for($i=0; $i<$n; $i++){
+    $result .= create_table_row($matriz[$i]);
+  }
+  $result .= "</table>";
+  return($result);
 }
 
 function power_matrix($n){
+  global $matriz;
+  for($i=0; $i<$n; $i++){
+    for($j=0; $j<=$n; $j++){
+      $matriz[$i][$j] = ($i+1)**($j+1);
+    }
+  }
+  return($matriz);
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
   <title>Teste PHP</title>
   <meta charset="utf-8">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
 <div class="container">
-  <h1>Tabela de potências</h1>
+  <h1 class="display-4">Tabela de potências</h1>
    <?php
     $n = 5;
     $m = power_matrix($n);
     echo create_table($m);
   ?>
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
+
